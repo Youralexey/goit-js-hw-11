c4acff95f216b3ec5a5cf3debf90d357b07db3a1
Youralexey <johndoe@example.com>
2021-08-04 00:03:12 +0300

    готово (без нотифликс библиотеки)

--- src/index.js
+++ src/index.js
@@ -1,80 +1,69 @@
-import './sass/main.scss'
-import Notiflix from 'notiflix';
+import './sass/main.scss';
 import _debounce from '../node_modules/lodash.debounce';
-// import apiService from './js/apiService';
+import ApiService from './js/apiService';
 import galleryItem from './templates/galleryItem.hbs';
-import * as basicLightbox from 'basiclightbox'
+import Notiflix from 'notiflix';
+import * as basicLightbox from '../node_modules/basiclightbox';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css'
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
     inputEl: document.querySelector('.search-form-input'),
     gallery: document.querySelector('.gallery'),
-    loadMoreButton: document.querySelector('.load-more-btn'),
+    loadMoreBtn: document.querySelector('.load-more-btn'),
 };
+const apiService = new ApiService();
+console.log('apiService :>> ', apiService);
 
-refs.loadMoreButton.addEventListener('click', searchForm);
-refs.gallery.addEventListener('click', onGalleryImgClick);
-
-
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '22688730-7478780a51f867bdd79495f14';
-export default class ApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-    };
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', loadMore);
+refs.gallery.addEventListener('click', onImgClick);
 
 
-    fetchImgs() {
-        const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
-        console.log(url)
-            return fetch(url).then(response => {
-            console.log('response :>> ', response);
-            if (!response.ok) {
-                 Notiflix.Report.Failure( 'Sorry, there are no images matching your search query. Please try again.', 'Ok' ); 
-                return;
-            } else {
-                return response.json();
-            }
-        }).then(data => {
-            this.incrementPage();
-            console.log(data.hits);
-            appendHits(data.hits)
-            return data.hits;
-        })
-    }
-
-    incrementPage() {
-        this.page += 1;
-    };
+function onSearch(e) {
+    e.preventDefault();
+    clearInput()
+    apiService.query = e.target.elements.query.value;
+    console.log('apiService.query :>> ', apiService.query);
+    // const total = response.totalHits;
     
-    get () {
-        return this.searchQuery;
-    };
-    set (newQuery) {
-        this.searchQuery = newQuery
-    };
-};
-
-let apiService = new ApiService();
-
-
-function searchForm(event) {
-    event.preventDefault();
-    apiService.set(refs.inputEl.value);
-    apiService.fetchImgs();
+    if (apiService.query) {
+        apiService.fetchImg().then(appendHitsMarckup);
+        refs.loadMoreBtn.classList.remove('is-hidden');
+    }
+    // if (apiService.query === '') {
+    //     return Notiflix.Report.Failure( 'Sorry, there are no images matching your search query','Please try again.' ); 
+    // }
 }
 
-function appendHits(hits) {
+function loadMore() {
+    apiService.fetchImg().then(appendHitsMarckup).then(scroll);
+}
+
+function appendHitsMarckup(hits) {
     refs.gallery.insertAdjacentHTML('beforeend', galleryItem(hits))
 }
 
-function onGalleryImgClick(event) {
-    console.log(event.target.nodeName)
-    if (event.target.nodeName === 'IMG') {
-        console.log(event.target.dataset.source)
-        const instance = basicLightbox.create(`
-    <img src=${event.target.dataset.source}>`);
+function scroll(){
+   const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 9,
+  behavior: 'smooth',
+});
+}
+
+function onImgClick(e) {
+    if (e.target.nodeName === 'IMG') {
+        console.log('e.target :>> ', e.target);
+        const instance = basicLightbox.create(`<img src=${e.target.dataset.source}>`);
         instance.show()
     }
+}
+
+function clearInput() {
+    refs.gallery.innerHTML = '';
+    refs.loadMoreBtn.classList.add('is-hidden')
 }
---
+++ src/js/apiService.js
@@ -0,0 +1,37 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '22688730-7478780a51f867bdd79495f14';
+
+export default class ApiService {
+    constructor() {
+        this.seachQuery = '';
+        this.page = 1;
+    }
+
+    fetchImg() {
+        const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.seachQuery}&page=${this.page}&per_page=40&key=${API_KEY}`;
+        console.log(url.totalHits)
+            return fetch(url).then(response => {
+            console.log('response :>> ', response);
+            if (!response.ok) {
+                return Notiflix.Report.Failure( 'Sorry, there are no images matching your search query','Please try again.' );
+            } else {
+                return response.json();
+            }
+        }).then(data => {
+            this.incrementPages();
+            return data.hits;
+        })
+    }
+    
+    incrementPages() {
+        this.page += 1;
+    }
+
+    get query() {
+        return this.seachQuery;
+    }
+
+    set query(newQuery) {
+        this.seachQuery = newQuery;
+    }
+}
